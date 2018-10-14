ec71ba401bc4ec382b6011bceecbf772a1e28734
Marc Chesebro <Marc.chesebro@gmail.com>
2018-10-14 14:31:27 -0400

    fixed tabs

--- ftp_server.java
+++ ftp_server.java
@@ -1,53 +1,50 @@
-import java.io.*; 
+import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public final class ftp_server {
-  public static void main(String argv[]) throws Exception {
-	
-            String fromClient;
-            String clientCommand;
-            byte[] data;
-            
-        
-            ServerSocket welcomeSocket = new ServerSocket(12000);
-            String frstln;
-          
-            while(true)
-            {
-                Socket connectionSocket = welcomeSocket.accept();
-                      
-                DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            
-                  fromClient = inFromClient.readLine();
-                 
-                  StringTokenizer tokens = new StringTokenizer(fromClient);
-                  frstln = tokens.nextToken();
-                  port = Integer.parseInt(frstln);
-                  clientCommand = tokens.nextToken();
-                  
-                  if(clientCommand.equals("list:"))
-                  { 
-                             
-                      Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
-                      DataOutputStream  dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
+    public static void main(String argv[]) throws Exception {
+
+        String fromClient;
+        String clientCommand;
+        byte[] data;
+
+
+        ServerSocket welcomeSocket = new ServerSocket(12000);
+        String frstln;
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            fromClient = inFromClient.readLine();
+
+            StringTokenizer tokens = new StringTokenizer(fromClient);
+            frstln = tokens.nextToken();
+            port = Integer.parseInt(frstln);
+            clientCommand = tokens.nextToken();
+
+            if (clientCommand.equals("list:")) {
+
+                Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
                       ..........................
 
-     
-                          }
 
-                           dataSocket.close();
-			   System.out.println("Data Socket closed");
-                     }
+            }
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
         
 			......................
-             
-                if(clientCommand.equals("retr:"))
-                {
+
+        if (clientCommand.equals("retr:")) {
                 ..............................
 		..............................
-		 }
-  }
+        }
+    }
 }
     
